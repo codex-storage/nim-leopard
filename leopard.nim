0d2fae23bfdd164c8dca85c4f992e73242ea9634
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-03-07 23:57:13 -0600

    initial implementation and tests

---
+++ leopard.nim
@@ -0,0 +1,307 @@
+import pkg/stew/ptrops
+import pkg/stew/results
+import pkg/upraises
+
+import ./leopard/wrapper
+
+export results
+
+push: {.upraises: [].}
+
+const
+  LeopardBadCodeMsg = "Bad RS code"
+  LeopardInconsistentSizeMsg =
+    "Buffer sizes must all be the same multiple of 64 bytes"
+  LeopardNeedLessDataMsg = "Too much recovery data received"
+  LeopardNotEnoughDataMsg = "Buffer counts are too low"
+
+  MinBufferSize* = 64.uint
+
+type
+  Data* = seq[seq[byte]]
+
+  LeopardDefect* = object of Defect
+
+  # It should not be necessary to redefine LeopardResult, but if that's not
+  # done here then defining LeopardError as `object of CatchableError` will
+  # cause a mystery crash at compile-time (symbol not found). Can workaround by
+  # defining as just `object`, but then when trying to work with LeopardResult
+  # errors in e.g. tests/test_leopard.nim the same mystery crash happens at
+  # compile-time. The problem may be related to use of importcpp in
+  # leopard/wrapper.nim, so it could be a compiler bug. By redefining
+  # LeopardResult in this module (and casting wrapper.LeopardResult values) the
+  # the problem is avoided.
+  LeopardResult* = enum
+    LeopardNotEnoughData    = -11.cint # Buffer counts are too low
+    LeopardNeedLessData     = -10.cint # Too much recovery data received
+    LeopardInconsistentSize =  -9.cint # Buffer sizes must all be the same multiple of 64 bytes
+    LeopardBadCode          =  -8.cint # Bad RS code
+    LeopardCallInitialize   = wrapper.LeopardCallInitialize
+    LeopardPlatform         = wrapper.LeopardPlatform
+    LeopardInvalidInput     = wrapper.LeopardInvalidInput
+    LeopardInvalidCounts    = wrapper.LeopardInvalidCounts
+    LeopardInvalidSize      = wrapper.LeopardInvalidSize
+    LeopardTooMuchData      = wrapper.LeopardTooMuchData
+    LeopardNeedMoreData     = wrapper.LeopardNeedMoreData
+    LeopardSuccess          = wrapper.LeopardSuccess
+
+  LeopardError* = object of CatchableError
+    code*: LeopardResult
+
+  ParityData* = Data
+
+  ReedSolomonCode* = tuple[codeword, data, parity: uint] # symbol counts
+
+# https://github.com/catid/leopard/issues/12
+# https://www.cs.cmu.edu/~guyb/realworld/reedsolomon/reed_solomon_codes.html
+#
+# RS(255,239)
+# ---------------------------------
+# codeword symbols = 255
+# data symbols     = 239
+# parity symbols   = 255 - 239 = 16
+
+proc RS*(codeword, data: Positive): ReedSolomonCode =
+  var
+    parity = codeword - data
+
+  if parity <= 0: parity = 0
+  (codeword: codeword.uint, data: data.uint, parity: parity.uint)
+
+when (NimMajor, NimMinor, NimPatch) < (1, 4, 0):
+  const
+    header = "<stdlib.h>"
+
+  proc c_malloc(size: csize_t): pointer {.importc: "malloc", header: header.}
+  proc c_free(p: pointer) {.importc: "free", header: header.}
+
+proc SIMDSafeAllocate(size: int): pointer {.inline.}  =
+  var
+    data =
+      when (NimMajor, NimMinor, NimPatch) < (1, 4, 0):
+        c_malloc(LEO_ALIGN_BYTES + size.uint)
+      else:
+        allocShared(LEO_ALIGN_BYTES + size.uint)
+
+    doffset = cast[uint](data) mod LEO_ALIGN_BYTES
+
+  data = offset(data, (LEO_ALIGN_BYTES + doffset).int)
+
+  var
+    offsetPtr = cast[pointer](cast[uint](data) - 1)
+
+  moveMem(offsetPtr, addr doffset, sizeof(doffset))
+  data
+
+proc SIMDSafeFree(data: pointer) {.inline.} =
+  var
+    data = data
+
+  if not data.isNil:
+    let
+      offset = cast[uint](data) - 1
+
+    if offset >= LEO_ALIGN_BYTES: return
+
+    data = cast[pointer](cast[uint](data) - (LEO_ALIGN_BYTES - offset))
+
+    when (NimMajor, NimMinor, NimPatch) < (1, 4, 0):
+      c_free data
+    else:
+      deallocShared data
+
+proc leoInit*() =
+  if wrapper.leoInit() != 0:
+    raise (ref LeopardDefect)(msg: "Leopard-RS failed to initialize")
+
+proc encode*(code: ReedSolomonCode, data: Data):
+    Result[ParityData, LeopardError] =
+  if code.parity < 1 or code.parity > code.data:
+    return err LeopardError(code: LeopardBadCode, msg: LeopardBadCodeMsg)
+
+  var
+    data = data
+
+  let
+    symbolBytes = data[0].len
+
+  if data.len < code.data.int:
+    return err LeopardError(code: LeopardNotEnoughData,
+      msg: LeopardNotEnoughDataMsg)
+
+  elif data.len > code.data.int:
+    return err LeopardError(code: LeopardTooMuchData,
+      msg: $leoResultString(wrapper.LeopardTooMuchData))
+
+  if symbolBytes < MinBufferSize.int or symbolBytes mod MinBufferSize.int != 0:
+    return err LeopardError(code: LeopardInvalidSize,
+      msg: $leoResultString(wrapper.LeopardInvalidSize))
+
+  var
+    enData = newSeq[pointer](code.data)
+
+  for i in 0..<code.data:
+    if data[i].len != symbolBytes:
+      for i in 0..<code.data: SIMDSafeFree enData[i]
+      return err LeopardError(code: LeopardInconsistentSize,
+        msg: LeopardInconsistentSizeMsg)
+
+    enData[i] = SIMDSafeAllocate symbolBytes
+    moveMem(enData[i], addr data[i][0], symbolBytes)
+
+  let
+    workCount = leoEncodeWorkCount(code.data.cuint, code.parity.cuint)
+
+  if workCount == 0:
+    for i in 0..<code.data: SIMDSafeFree enData[i]
+    return err LeopardError(code: LeopardInvalidInput,
+      msg: $leoResultString(wrapper.LeopardInvalidInput))
+
+  var
+    workData = newSeq[pointer](workCount)
+
+  for i in 0..<workCount:
+    workData[i] = SIMDSafeAllocate symbolBytes
+
+  let
+    encodeRes = leoEncode(
+      symbolBytes.uint64,
+      code.data.cuint,
+      code.parity.cuint,
+      workCount,
+      addr enData[0],
+      addr workData[0]
+    )
+
+  if encodeRes != wrapper.LeopardSuccess:
+    for i in 0..<code.data: SIMDSafeFree enData[i]
+    for i in 0..<workCount: SIMDSafeFree workData[i]
+    return err LeopardError(code: cast[LeopardResult](encodeRes),
+      msg: $leoResultString(encodeRes))
+
+  var
+    parityData: ParityData
+
+  newSeq(parityData, code.parity)
+  for i in 0..<code.parity:
+    newSeq(parityData[i], symbolBytes)
+    moveMem(addr parityData[i][0], workData[i], symbolBytes)
+
+  for i in 0..<code.data: SIMDSafeFree enData[i]
+  for i in 0..<workCount: SIMDSafeFree workData[i]
+
+  ok parityData
+
+proc decode*(code: ReedSolomonCode, data: Data, parityData: ParityData,
+    symbolBytes: uint): Result[Data, LeopardError] =
+  if code.parity < 1 or code.parity > code.data:
+    return err LeopardError(code: LeopardBadCode, msg: LeopardBadCodeMsg)
+
+  var
+    data = data
+    parityData = parityData
+    holes: seq[int]
+
+  if data.len < code.data.int:
+    return err LeopardError(code: LeopardNotEnoughData,
+      msg: LeopardNotEnoughDataMsg)
+
+  elif data.len > code.data.int:
+    return err LeopardError(code: LeopardTooMuchData,
+      msg: $leoResultString(wrapper.LeopardTooMuchData))
+
+  if parityData.len < code.parity.int:
+    return err LeopardError(code: LeopardNeedMoreData,
+      msg: $leoResultString(wrapper.LeopardNeedMoreData))
+
+  elif parityData.len > code.parity.int:
+    return err LeopardError(code: LeopardNeedLessData,
+      msg: LeopardNeedLessDataMsg)
+
+  if symbolBytes < MinBufferSize or symbolBytes mod MinBufferSize != 0:
+    return err LeopardError(code: LeopardInvalidSize,
+      msg: $leoResultString(wrapper.LeopardInvalidSize))
+
+  var
+    deData = newSeq[pointer](code.data)
+
+  for i in 0..<code.data:
+    if data[i].len != 0:
+      if data[i].len != symbolBytes.int:
+        for i in 0..<code.data: SIMDSafeFree deData[i]
+        return err LeopardError(code: LeopardInconsistentSize,
+          msg: LeopardInconsistentSizeMsg)
+
+      deData[i] = SIMDSafeAllocate symbolBytes.int
+      moveMem(deData[i], addr data[i][0], symbolBytes)
+
+    else:
+      holes.add i.int
+
+  if holes.len == 0:
+    for i in 0..<code.data: SIMDSafeFree deData[i]
+    return ok data
+
+  var
+    paData = newSeq[pointer](code.parity)
+
+  for i in 0..<code.parity:
+    if parityData[i].len != 0:
+      if parityData[i].len != symbolBytes.int:
+        for i in 0..<code.data: SIMDSafeFree deData[i]
+        for i in 0..<code.parity: SIMDSafeFree paData[i]
+        return err LeopardError(code: LeopardInconsistentSize,
+          msg: LeopardInconsistentSizeMsg)
+
+      paData[i] = SIMDSafeAllocate symbolBytes.int
+      moveMem(paData[i], addr parityData[i][0], symbolBytes)
+
+  let
+    workCount = leoDecodeWorkCount(code.data.cuint, code.parity.cuint)
+
+  if workCount == 0:
+    for i in 0..<code.data: SIMDSafeFree deData[i]
+    for i in 0..<code.parity: SIMDSafeFree paData[i]
+    return err LeopardError(code: LeopardInvalidInput,
+      msg: $leoResultString(wrapper.LeopardInvalidInput))
+
+  var
+    workData = newSeq[pointer](workCount)
+
+  for i in 0..<workCount:
+    workData[i] = SIMDSafeAllocate symbolBytes.int
+
+  let
+    decodeRes = leoDecode(
+      symbolBytes.uint64,
+      code.data.cuint,
+      code.parity.cuint,
+      workCount,
+      addr deData[0],
+      addr paData[0],
+      addr workData[0]
+    )
+
+  if decodeRes != wrapper.LeopardSuccess:
+    for i in 0..<code.data: SIMDSafeFree deData[i]
+    for i in 0..<code.parity: SIMDSafeFree paData[i]
+    for i in 0..<workCount: SIMDSafeFree workData[i]
+    return err LeopardError(code: cast[LeopardResult](decodeRes),
+      msg: $leoResultString(decodeRes))
+
+  var
+    recoveredData: Data
+
+  newSeq(recoveredData, workCount)
+  for i in 0..<workCount:
+    newSeq(recoveredData[i], symbolBytes)
+    moveMem(addr recoveredData[i][0], workData[i], symbolBytes)
+
+  for i in holes:
+    data[i] = recoveredData[i]
+
+  for i in 0..<code.data: SIMDSafeFree deData[i]
+  for i in 0..<code.parity: SIMDSafeFree paData[i]
+  for i in 0..<workCount: SIMDSafeFree workData[i]
+
+  ok data
---
+++ leopard/wrapper.nim
@@ -0,0 +1,293 @@
+## Copyright (c) 2017 Christopher A. Taylor.  All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+## * Redistributions of source code must retain the above copyright notice,
+##   this list of conditions and the following disclaimer.
+## * Redistributions in binary form must reproduce the above copyright notice,
+##   this list of conditions and the following disclaimer in the documentation
+##   and/or other materials provided with the distribution.
+## * Neither the name of Leopard-RS nor the names of its contributors may be
+##   used to endorse or promote products derived from this software without
+##   specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
+
+
+## Leopard-RS
+## MDS Reed-Solomon Erasure Correction Codes for Large Data in C
+##
+## Algorithms are described in LeopardCommon.h
+##
+##
+## Inspired by discussion with:
+##
+## Sian-Jhen Lin <sjhenglin@gmail.com> : Author of {1} {3}, basis for Leopard
+## Bulat Ziganshin <bulat.ziganshin@gmail.com> : Author of FastECC
+## Yutaka Sawada <tenfon@outlook.jp> : Author of MultiPar
+##
+##
+## References:
+##
+## {1} S.-J. Lin, T. Y. Al-Naffouri, Y. S. Han, and W.-H. Chung,
+## "Novel Polynomial Basis with Fast Fourier Transform
+## and Its Application to Reed-Solomon Erasure Codes"
+## IEEE Trans. on Information Theory, pp. 6284-6299, November, 2016.
+##
+## {2} D. G. Cantor, "On arithmetical algorithms over finite fields",
+## Journal of Combinatorial Theory, Series A, vol. 50, no. 2, pp. 285-300, 1989.
+##
+## {3} Sian-Jheng Lin, Wei-Ho Chung, "An Efficient (n, k) Information
+## Dispersal Algorithm for High Code Rate System over Fermat Fields,"
+## IEEE Commun. Lett., vol.16, no.12, pp. 2036-2039, Dec. 2012.
+##
+## {4} Plank, J. S., Greenan, K. M., Miller, E. L., "Screaming fast Galois Field
+## arithmetic using Intel SIMD instructions."  In: FAST-2013: 11th Usenix
+## Conference on File and Storage Technologies, San Jose, 2013
+
+
+import upraises
+push: {.upraises: [].}
+
+
+## -----------------------------------------------------------------------------
+## Build configuration
+
+import std/compilesettings
+import std/os
+import std/strutils
+
+const
+  LeopardCmakeFlags {.strdefine.} =
+    when defined(macosx):
+      "-DCMAKE_BUILD_TYPE=Release -DENABLE_OPENMP=off"
+    elif defined(windows):
+      "-G\"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release"
+    else:
+      "-DCMAKE_BUILD_TYPE=Release"
+
+  LeopardDir {.strdefine.} =
+    joinPath(currentSourcePath.parentDir.parentDir, "vendor", "leopard")
+
+  buildDir = joinPath(querySetting(nimcacheDir), "vendor_leopard")
+
+  LeopardHeader {.strdefine.} = "leopard.h"
+
+  LeopardLib {.strdefine.} = joinPath(buildDir, "liblibleopard.a")
+
+  LeopardCompilerFlags {.strdefine.} =
+    when defined(macosx):
+      "-I" & LeopardDir
+    else:
+      "-I" & LeopardDir & " -fopenmp"
+
+  LeopardLinkerFlags {.strdefine.} =
+    when defined(macosx):
+      LeopardLib
+    else:
+      LeopardLib & " -fopenmp"
+
+  LeopardExtraCompilerFlags {.strdefine.} = ""
+
+  LeopardExtraLinkerFlags {.strdefine.} = ""
+
+static:
+  if defined(windows):
+    func pathUnix2Win(path: string): string =
+      gorge("cygpath -w " & path.strip).strip
+
+    func pathWin2Unix(path: string): string =
+      gorge("cygpath " & path.strip).strip
+
+    proc bash(cmd: varargs[string]): string =
+      gorge(gorge("which bash").pathUnix2Win & " -c '" & cmd.join(" ") & "'")
+
+    proc bashEx(cmd: varargs[string]): tuple[output: string, exitCode: int] =
+      gorgeEx(gorge("which bash").pathUnix2Win & " -c '" & cmd.join(" ") & "'")
+
+    let
+      buildDirUnix = buildDir.pathWin2Unix
+      leopardDirUnix = LeopardDir.pathWin2Unix
+    if defined(LeopardRebuild): discard bash("rm -rf", buildDirUnix)
+    if (bashEx("ls", LeopardLib.pathWin2Unix)).exitCode != 0:
+      discard bash("mkdir -p", buildDirUnix)
+      let cmd =
+        @["cd", buildDirUnix, "&& cmake", leopardDirUnix, LeopardCmakeFlags,
+          "&& make"]
+      echo "\nBuilding Leopard-RS: " & cmd.join(" ")
+      let (output, exitCode) = bashEx cmd
+      echo output
+      if exitCode != 0:
+        discard bash("rm -rf", buildDirUnix)
+        raise (ref Defect)(msg: "Failed to build Leopard-RS")
+  else:
+    if defined(LeopardRebuild): discard gorge "rm -rf " & buildDir
+    if gorgeEx("ls " & LeopardLib).exitCode != 0:
+      discard gorge "mkdir -p " & buildDir
+      let cmd =
+        "cd " & buildDir & " && cmake " & LeopardDir & " " & LeopardCmakeFlags &
+        " && make"
+      echo "\nBuilding Leopard-RS: " & cmd
+      let (output, exitCode) = gorgeEx cmd
+      echo output
+      if exitCode != 0:
+        discard gorge "rm -rf " & buildDir
+        raise (ref Defect)(msg: "Failed to build Leopard-RS")
+
+{.passC: LeopardCompilerFlags & " " & LeopardExtraCompilerFlags.}
+{.passL: LeopardLinkerFlags & " " & LeopardExtraLinkerFlags.}
+
+{.pragma: leo, cdecl, header: LeopardHeader.}
+
+
+## -----------------------------------------------------------------------------
+## Library version
+
+var LEO_VERSION* {.header: LeopardHeader, importc.}: int
+
+
+## -----------------------------------------------------------------------------
+## Platform/Architecture
+
+# maybe should detect AVX2 and set to 32 if detected, 16 otherwise:
+# https://github.com/catid/leopard/blob/master/LeopardCommon.h#L247-L253
+# https://github.com/mratsim/Arraymancer/blob/master/src/arraymancer/laser/cpuinfo_x86.nim#L220
+const LEO_ALIGN_BYTES* = 16
+
+
+## -----------------------------------------------------------------------------
+## Initialization API
+
+## leoInit()
+##
+## Perform static initialization for the library, verifying that the platform
+## is supported.
+##
+## Returns 0 on success and other values on failure.
+
+proc leoInit*(): cint {.leo, importcpp: "leo_init".}
+
+
+## -----------------------------------------------------------------------------
+## Shared Constants / Datatypes
+
+## Results
+type
+  LeopardResult* = enum
+    LeopardCallInitialize = -7.cint ## Call leoInit() first
+    LeopardPlatform       = -6.cint ## Platform is unsupported
+    LeopardInvalidInput   = -5.cint ## A function parameter was invalid
+    LeopardInvalidCounts  = -4.cint ## Invalid counts provided
+    LeopardInvalidSize    = -3.cint ## Buffer size must be multiple of 64 bytes
+    LeopardTooMuchData    = -2.cint ## Buffer counts are too high
+    LeopardNeedMoreData   = -1.cint ## Not enough recovery data received
+    LeopardSuccess        =  0.cint ## Operation succeeded
+
+## Convert Leopard result to string
+func leoResultString*(res: LeopardResult): cstring
+  {.leo, importc: "leo_result_string".}
+
+
+## -----------------------------------------------------------------------------
+## Encoder API
+
+## leoEncodeWorkCount()
+##
+## Calculate the number of work data buffers to provide to leoEncode().
+##
+## The sum of originalCount + recoveryCount must not exceed 65536.
+##
+## Returns the workCount value to pass into leoEncode().
+## Returns 0 on invalid input.
+
+func leoEncodeWorkCount*(originalCount, recoveryCount: cuint): cuint
+  {.leo, importc: "leo_encode_work_count".}
+
+## leoEncode()
+##
+## Generate recovery data.
+##
+## bufferBytes:   Number of bytes in each data buffer.
+## originalCount: Number of original data buffers provided.
+## recoveryCount: Number of desired recovery data buffers.
+## workCount:     Number of work data buffers, from leoEncodeWorkCount().
+## originalData:  Array of pointers to original data buffers.
+## workData:      Array of pointers to work data buffers.
+##
+## The sum of originalCount + recoveryCount must not exceed 65536.
+## The recoveryCount <= originalCount.
+##
+## The value of bufferBytes must be a multiple of 64.
+## Each buffer should have the same number of bytes.
+## Even the last piece must be rounded up to the block size.
+##
+## Returns LeopardSuccess on success.
+## The first set of recoveryCount buffers in workData will be the result.
+## Returns other values on errors.
+
+proc leoEncode*(
+  bufferBytes: uint64,   ## Number of bytes in each data buffer
+  originalCount: cuint,  ## Number of originalData[] buffer pointers
+  recoveryCount: cuint,  ## Number of recovery data buffer pointers
+                         ## (readable post-call from start of workData[])
+  workCount: cuint,      ## Number of workData[] buffer pointers
+  originalData: pointer, ## Array of pointers to original data buffers
+  workData: pointer,     ## Array of pointers to work data buffers
+): LeopardResult {.leo, importc: "leo_encode".}
+
+
+## -----------------------------------------------------------------------------
+## Decoder API
+
+## leoDecodeWorkCount()
+##
+## Calculate the number of work data buffers to provide to leoDecode().
+##
+## The sum of originalCount + recoveryCount must not exceed 65536.
+##
+## Returns the workCount value to pass into leoDecode().
+## Returns 0 on invalid input.
+
+func leoDecodeWorkCount*(originalCount, recoveryCount: cuint): cuint
+  {.leo, importc: "leo_decode_work_count".}
+
+## leoDecode()
+##
+## Decode original data from recovery data.
+##
+## bufferBytes:   Number of bytes in each data buffer.
+## originalCount: Number of original data buffers provided.
+## recoveryCount: Number of recovery data buffers provided.
+## workCount:     Number of work data buffers, from leoDecodeWorkCount().
+## originalData:  Array of pointers to original data buffers.
+## recoveryData:  Array of pointers to recovery data buffers.
+## workData:      Array of pointers to work data buffers.
+##
+## Lost original/recovery data should be set to NULL.
+##
+## The sum of recoveryCount + the number of non-NULL original data must be at
+## least originalCount in order to perform recovery.
+##
+## Returns LeopardSuccess on success.
+## Returns other values on errors.
+
+proc leoDecode*(
+  bufferBytes: uint64,   ## Number of bytes in each data buffer
+  originalCount: cuint,  ## Number of originalData[] buffer pointers
+  recoveryCount: cuint,  ## Number of recoveryData[] buffer pointers
+  workCount: cuint,      ## Number of workData[] buffer pointers
+  originalData: pointer, ## Array of pointers to original data buffers
+  recoveryData: pointer, ## Array of pointers to recovery data buffers
+  workData: pointer,     ## Array of pointers to work data buffers
+): LeopardResult {.leo, importc: "leo_decode".}
